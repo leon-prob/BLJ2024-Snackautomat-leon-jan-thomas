3c9a3a4d1eaf74b8710e160bcabf4e508173de97
Jan Ludwig <jan.ludwig@noseryoung.ch>
2025-02-21 11:02:50 +0100

    implementing main Menu

--- Snackautomat/src/blj/noseryoung/ch/Main.java
+++ Snackautomat/src/blj/noseryoung/ch/Main.java
@@ -1,5 +1,8 @@
+package blj.noseryoung.ch;
+
+;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        menu();
     }
 }
---
+++ Snackautomat/src/blj/noseryoung/ch/Menu.java
@@ -0,0 +1,55 @@
+package blj.noseryoung.ch;
+
+import java.util.Scanner;
+
+public class Menu {
+    public void mainMenu() {
+        Scanner main = new Scanner(System.in);
+        System.out.println("Welcome to THE VENDING MACHINE");
+        System.out.println(
+                "###########################\n" +
+                        "# What do you wish to do? #\n" +
+                        "###########################\n" +
+                        "# Show Purse           1  #\n" +
+                        "# Show all Products    2  #\n" +
+                        "# Buy a Product        3  #\n" +
+                        "# Exit                 4  #\n" +
+                        "###########################\n");
+        String mainMenu = main.nextLine();
+        switch (mainMenu) {
+            case "1":
+                break;
+            //Show Purse function
+            case "2":
+                //Show all Products function
+                break;
+            case "3":
+                initBuyProduct();
+                break;
+            case "4":
+                //Exit function
+                break;
+            default:
+                //Input not Valid function
+                break;
+        }
+    }
+    private void initBuyProduct() {
+        System.out.println(
+                        "###########################\n" +
+                        "# _______________________ #\n" +
+                        "# | [01] [02] [03] [04] | #\n" +
+                        "# | [05] [06] [07] [08] | #\n" +
+                        "# | [09] [10] [11] [12] | #\n" +
+                        "# | [13] [14] [15] [16] | #\n" +
+                        "# | [17] [18] [19] [20] | #\n" +
+                        "# |_____________________| #\n" +
+                        "#                         #\n" +
+                        "#       [======]          #\n" +
+                        "#       [______]          #\n" +
+                        "###########################\n" +
+                        "# Choose a number from    #\n" +
+                        "# above (01-20)           #\n" +
+                        "###########################\n");
+    }
+}
